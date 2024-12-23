62698c57eb21787ee0ccb172d49a57f3ce858fbf
NarmalaSk <shashinarmala29@gmail.com>
2024-12-23 21:42:39 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,11 @@
 from app import app
 from waitress import serve
+import os
 
 if __name__ == "__main__":
-    serve(app, host='127.0.0.1', port=8080)
+    # Use waitress for local development
+    if os.getenv("FLASK_ENV") == "development":
+        serve(app, host='127.0.0.1', port=8080)
+    else:
+        # You can use gunicorn for production deployment, but it's handled by the platform
+        app.run(host='0.0.0.0', port=8080)
